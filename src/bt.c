dba46a536224ab2d82c41e6025c2ee02c63fe4d7
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2023-11-11 23:51:29 +0100

    Add web UI

--- src/bt.c
+++ src/bt.c
@@ -10,13 +10,7 @@
 
 // Project
 #include "io.h"
-#include "lumonade.h" // Generated from lumonade.gatt
-
-// TODO(eustas): where does it come from?
-extern uint8_t const profile_data[];
-
-// TODO(eustas): this is a toy characteristic.
-static uint16_t magic_smoke = 42;
+#include "lumo.h" // Generated from lumo.gatt
 
 // TODO(eustas): explain
 int le_notification_enabled;
@@ -40,7 +34,7 @@ static uint8_t adv_data[] = {
 };
 static const uint8_t adv_data_len = sizeof(adv_data);
 
-#define HEARTBEAT_PERIOD_MS 500
+static int heartbeat_period_ms = 500;
 static btstack_timer_source_t heartbeat;
 
 static btstack_packet_callback_registration_t hci_event_callback_registration;
@@ -50,12 +44,11 @@ static void heartbeat_handler(struct btstack_timer_source *ts) {
   counter++;
 
   // Update the temp every 10s
-  if (counter % 10 == 0) {
-    // produce_magic_smoke();
-    if (le_notification_enabled) {
-      att_server_request_can_send_now_event(con_handle);
-    }
-  }
+  //if (counter % 10 == 0) {
+  //  if (le_notification_enabled) {
+  //    att_server_request_can_send_now_event(con_handle);
+  //  }
+  //}
 
   // Invert the led
   static int led_on = true;
@@ -63,13 +56,13 @@ static void heartbeat_handler(struct btstack_timer_source *ts) {
   cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
 
   // Restart timer
-  btstack_run_loop_set_timer(ts, HEARTBEAT_PERIOD_MS);
+  btstack_run_loop_set_timer(ts, heartbeat_period_ms);
   btstack_run_loop_add_timer(ts);
 }
 
 // Shortcut.
-#define HANDLE(S)                                                              \
-  ATT_CHARACTERISTIC_ORG_BLUETOOTH_CHARACTERISTIC_TEMPERATURE_01_##S##_HANDLE
+#define IO_VALUE_HANDLE \
+  ATT_CHARACTERISTIC_ORG_BLUETOOTH_CHARACTERISTIC_STRING_01_VALUE_HANDLE
 
 void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet,
                     uint16_t size) {
@@ -99,18 +92,16 @@ void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet,
     assert(adv_data_len <= 31); // ble limitation
     gap_advertisements_set_data(adv_data_len, (uint8_t *)adv_data);
     gap_advertisements_enable(1);
-
-    // produce_magic_smoke();
     break;
 
   case HCI_EVENT_DISCONNECTION_COMPLETE:
     le_notification_enabled = 0;
     break;
 
-  case ATT_EVENT_CAN_SEND_NOW:
-    att_server_notify(con_handle, HANDLE(VALUE), (uint8_t *)&magic_smoke,
-                      sizeof(magic_smoke));
-    break;
+  //case ATT_EVENT_CAN_SEND_NOW:
+  //  att_server_notify(con_handle, HANDLE(VALUE), (uint8_t *)&magic_smoke,
+  //                    sizeof(magic_smoke));
+  //  break;
 
   default:
     break;
@@ -122,9 +113,10 @@ uint16_t att_read_callback(hci_con_handle_t connection_handle,
                            uint8_t *buffer, uint16_t buffer_size) {
   (void)connection_handle;
 
-  if (att_handle == HANDLE(VALUE)) {
-    return att_read_callback_handle_blob((const uint8_t *)&magic_smoke,
-                                         sizeof(magic_smoke), offset, buffer,
+  if (att_handle == IO_VALUE_HANDLE) {
+    const char str[] = "Abracadabra";
+    return att_read_callback_handle_blob((const uint8_t *)&str,
+                                         sizeof(str), offset, buffer,
                                          buffer_size);
   }
   return 0;
@@ -134,18 +126,23 @@ int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_handle,
                        uint16_t transaction_mode, uint16_t offset,
                        uint8_t *buffer, uint16_t buffer_size) {
   (void)transaction_mode;
-  (void)offset;
-  (void)buffer_size;
 
-  if (att_handle != HANDLE(CLIENT_CONFIGURATION))
+  // Technically offset can be any, but its semantics is unclear. Given what
+  // I see in the implementation, it should be zero.
+  if (offset != 0) {
     return 0;
-  le_notification_enabled =
-      little_endian_read_16(buffer, 0) ==
-      GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION;
-  con_handle = connection_handle;
-  if (le_notification_enabled) {
-    att_server_request_can_send_now_event(con_handle);
   }
+
+  if (att_handle == IO_VALUE_HANDLE) {
+    if (buffer_size > 0) {
+      uint8_t c = buffer[0];
+      if (c >= '1' && c <= '9') {
+        heartbeat_period_ms = 100 * (c - '0');
+      }
+    }
+    return 0;
+  }
+
   return 0;
 }
 
@@ -161,6 +158,7 @@ void init_bt(void) {
   sm_init();
 
   // Setup (G)ATT server.
+  // profile_data is generated from .gatt file
   att_server_init(profile_data, att_read_callback, att_write_callback);
 
   // Inform about BTstack state.
@@ -172,7 +170,7 @@ void init_bt(void) {
 
   // set one-shot btstack timer
   heartbeat.process = &heartbeat_handler;
-  btstack_run_loop_set_timer(&heartbeat, HEARTBEAT_PERIOD_MS);
+  btstack_run_loop_set_timer(&heartbeat, heartbeat_period_ms);
   btstack_run_loop_add_timer(&heartbeat);
 
   // turn on bluetooth!
